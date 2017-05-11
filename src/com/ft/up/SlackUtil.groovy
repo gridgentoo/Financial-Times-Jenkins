05560dc5dea0eeb76625b12388d4d1871fd0c05d
Sorin Buliarca <Sorin.Buliarca@iquestgroup.com>
2017-05-11 09:53:10 +0300

    Updated Slack util.

--- src/com/ft/up/SlackUtil.groovy
+++ src/com/ft/up/SlackUtil.groovy
@@ -1,22 +1,25 @@
 package com.ft.up
-
-import java.nio.charset.StandardCharsets
-
 /**
- * Utis for Slack
+ * Utils for Slack
  */
 
+/**
+ * Sends a slack notification using the https://api.slack.com/methods/chat.postMessage method.
+ *
+ * @param channel the channel where to send the notification. Example: @username, #upp-tech
+ * @param message The message to send
+ * @param credentialId the id of Jenkins credentials to use. By default it will use 'ft.slack.bot-credentials'
+ */
 void sendSlackNotification(String channel, String message, String credentialId = "ft.slack.bot-credentials") {
-
-  String encodedChannel = URLEncoder.encode(channel, StandardCharsets.UTF_8.name())
-  String encodedMessage = URLEncoder.encode(message, StandardCharsets.UTF_8.name())
+  String encodedChannel = URLEncoder.encode(channel, "UTF-8")
+  String encodedMessage = URLEncoder.encode(message, "UTF-8")
   withCredentials([[$class: 'StringBinding', credentialsId: credentialId, variable: 'SLACK_TOKEN']]) {
     sh """
-    curl -X POST \\
+    curl -X POST -s \\
   https://slack.com/api/chat.postMessage \\
   -H 'cache-control: no-cache' \\
   -H 'content-type: application/x-www-form-urlencoded' \\
-  -d 'text=${encodedMessage}&token=${env.SLACK_TOKEN}&channel=${encodedChannel}'
+  -d 'text=${encodedMessage}&token=${env.SLACK_TOKEN}&channel=${encodedChannel}&username=jenkins-bot'
   """
   }
 }
