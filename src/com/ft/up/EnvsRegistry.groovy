e1d459e374987c0302ec5ab691541c12824d8785
Sorin Buliarca <sorin.buliarca@ft.com>
2017-05-19 16:50:49 +0300

    Added new env for xp-pub

--- src/com/ft/up/EnvsRegistry.groovy
+++ src/com/ft/up/EnvsRegistry.groovy
@@ -3,16 +3,18 @@ package com.ft.up
 class EnvsRegistry implements Serializable {
 
   public static final Map<String, String> envToApiServerMap = [
-      "xp"  : "https://k8s-delivery-upp-eu-api.ft.com",
-      "test": "https://k8s-delivery-upp-eu-api.ft.com"
+      "xp"    : "https://k8s-delivery-upp-eu-api.ft.com",
+      "pub-xp": "https://k8s-pub-upp-eu-api.ft.com",
+      "test"  : "https://k8s-delivery-upp-eu-api.ft.com"
   ]
 
   public static final Map<String, String> envToSlackChannelMap = [
-      "xp"  : "k8s_ro"
+      "xp"    : "k8s_ro",
+      "pub-xp": "k8s_ro"
   ]
 
   public static String getApiServerForEnv(String env) {
-    return  envToApiServerMap[env]
+    return envToApiServerMap[env]
   }
 
   public static String getSlackChannelForEnv(String env) {
