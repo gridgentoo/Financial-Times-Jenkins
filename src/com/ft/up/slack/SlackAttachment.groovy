dfc018388548af509b6073f7ef0a39573fb34404
Sorin Buliarca <sorin.buliarca@ft.com>
2017-05-25 10:37:36 +0300

    Added support for thumbUrl in the Slack attachment.

--- src/com/ft/up/slack/SlackAttachment.groovy
+++ src/com/ft/up/slack/SlackAttachment.groovy
@@ -4,7 +4,7 @@ package com.ft.up.slack
  * Replicates the structure from here: SlackAttachment attachment = new SlackAttachment()
  */
 class SlackAttachment implements Serializable {
-  String text
+  String text = ""
   String preText = ""
   String title = ""
   String titleUrl = ""
@@ -12,6 +12,7 @@ class SlackAttachment implements Serializable {
   String footer = ""
   String footerIcon = ""
   String imageUrl = ""
+  String thumbUrl = "http://www.tothenew.com/blog/wp-content/uploads/2016/08/w.png"
   String authorName = ""
   String authorLink = ""
   String authorIcon = ""
