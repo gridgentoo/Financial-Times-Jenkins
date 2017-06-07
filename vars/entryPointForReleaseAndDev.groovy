53a58a2f887d38e82710755067be174f5eb5672a
Sorin Buliarca <sorin.buliarca@ft.com>
2017-06-07 22:09:31 +0300

    Updated javadoc.

--- vars/entryPointForReleaseAndDev.groovy
+++ vars/entryPointForReleaseAndDev.groovy
@@ -3,13 +3,11 @@ import com.ft.jenkins.git.GitUtils
 import com.ft.jenkins.git.GithubReleaseInfo
 
 /**
- * Entry point that decides which pipeline to execute, based on the branch type to build.
- * For regular branches it will trigger the pipeline that automatically deploys to team environments,
- * and for tags, meaning we're dealing with releases, it will trigger the pipeline for continuous delivery in upper environments.
+ * Entry point that decides which pipeline to execute, based on the Github release type to build.
+ * For Github pre-releases it will trigger the pipeline that automatically deploys to team environments,
+ * and for GH releases it will trigger the pipeline for continuous delivery in upper environments.
  *
- * We need this entry point in order to be able to trigger builds when git tags are created in a multibranch fashion.
- * Since jenkins supports multibranch pipelines with a single Jenkinsfile entry point per git repository, I had to do this in order to
- * have 2 multibranch pipelines per repository. One will be for team environments deployments on every commit and one for release deployments from tags.
+ * This multibranch pipeline is designed to be triggered only for tags comming from Github releases.
  */
 def call(BuildConfig config) {
   GitUtils gitUtils = new GitUtils()
