82e37df0f8d9cee14fd1f67b9ed585547cfba33a
David Balazs <david.balazs@ft.com>
2017-06-29 10:06:47 +0300

    Updated doc

--- src/com/ft/jenkins/exceptions/InvalidAppConfigFileNameException.java
+++ src/com/ft/jenkins/exceptions/InvalidAppConfigFileNameException.java
@@ -1,7 +1,10 @@
 package com.ft.jenkins.exceptions;
 
 /**
- * Exception t
+ * Exception to be thrown if the name of a helm config file under `apps-config` does not meet the naming conventions:
+ * <SERVICE_NAME>_<CLUSTER_NAME>[_<ENVIRONMENT_NAME>].yaml.
+ * Examples:
+ *  - publish-availability-monitor_publishing_k8s.yaml
  */
 public class InvalidAppConfigFileNameException extends Exception {
   public InvalidAppConfigFileNameException(String message) {
