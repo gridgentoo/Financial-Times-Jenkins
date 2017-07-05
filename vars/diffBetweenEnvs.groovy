edbdf73bfadd481d344b6005e848e4ce849e2c6b
David Balazs <david.balazs@ft.com>
2017-07-05 14:08:13 +0300

    Added diff between envs

--- vars/diffBetweenEnvs.groovy
+++ vars/diffBetweenEnvs.groovy
@@ -8,72 +8,94 @@ def call(String firstEnvName, String secondEnvName, String clusterName) {
   echo "Diff between envs with params: [envToSyncFrom: ${firstEnvName}, envToBeSynced: ${secondEnvName}, cluster: ${clusterName}]"
   Environment envToSyncFrom = EnvsRegistry.getEnvironment(firstEnvName)
   Environment envToBeSynced = EnvsRegistry.getEnvironment(secondEnvName)
-  Cluster cluster = Cluster.valueOf(clusterName)
+  Cluster cluster = clusterName.toUpperCase()
 
-  Map<String, String> outdatedServices
-  def selectedServicesToBeSynced
+  Map<String, String> firstEnvCharts
+  Map<String, String> secondEnvCharts
+  List<String> addedCharts
+  List<String> modifiedCharts
+  List<String> removedCharts
+  Map<String, Boolean> choosenParamsForAddedCharts, choosenParamsForModifiedCharts, choosenParamsForRemovedCharts
 
   node('') {
     catchError {
       timeout(30) { //  timeout after 30 mins to not block jenkins
 
         stage('diff-envs') {
-          outdatedServices = doDiff(envToSyncFrom, envToBeSynced, cluster)
+          echo "Diff the clusters."
+          firstEnvCharts = getChartsFromEnv(envToSyncFrom, cluster)
+          secondEnvCharts = getChartsFromEnv(envToBeSynced, cluster)
+          
+          removedCharts = diffBetweenMaps(firstEnvCharts, secondEnvCharts)
+          addedCharts = diffBetweenMaps(secondEnvCharts, firstEnvCharts)
+          modifiedCharts = getModifiedCharts(firstEnvCharts, secondEnvCharts)
         }
 
-        stage('select-services-to-be-synced') {
-          String syncServicesMessage = "Added services"
-          def syncChoices = []
-          Set<String> servicesToBeSynced = outdatedServices.keySet()
-          for (int i = 0; i < servicesToBeSynced.size(); i++) {
-            syncChoices[i] = booleanParam(defaultValue: false,
-                                          description: '',
-                                          name: servicesToBeSynced.getAt(i))
-          }
-
-          selectedServicesToBeSynced = input(message: syncServicesMessage,
-                                             parameters: syncChoices,
-                                             ok: "Sync services")
-          echo "selectedServicesToBeSynced: ${selectedServicesToBeSynced}"
+        stage('select-charts-to-be-synced') {
+          choosenParamsForAddedCharts = getUserInputs(addedCharts)
+          choosenParamsForModifiedCharts = getUserInputs(modifiedCharts)
+          choosenParamsForRemovedCharts = getUserInputs(removedCharts)
         }
 
         stage('sync-services') {
-          for (int i = 0; i < selectedServicesToBeSynced.length; i++) {
-            echo "syncing service ${selectedServicesToBeSynced[i]}"
-          }
-        }
-
-        catchError {
-          echo "An error occurred in the pipeline."
-        }
-
-        stage("cleanup") {
-          cleanWs()
+          updateCharts(choosenParamsForAddedCharts)
+          updateCharts(choosenParamsForModifiedCharts)
+          updateCharts(choosenParamsForRemovedCharts)
         }
       }
+    }
+
+    catchError {
+      echo "An error occurred in the pipeline."
+    }
+
+    stage("cleanup") {
+      cleanWs()
     }
   }
 }
 
-public Map<String, String> doDiff(Environment firstEnv, Environment secondEnv, Cluster cluster) {
-  echo "Diff the clusters."
-  Map<String, String> firstEnvCharts, secondEnvCharts
+private void updateCharts(Map<String, Boolean> choosenParams) {
+  echo "Syncing services started by ${choosenParams.get('approver')}"
+  choosenParams.remove('approver')
+  Set<String> chartsToBeSynced = choosenParams.keySet()
+  for (int i = 0; i < chartsToBeSynced.size(); i++) {
+    String chartToBeSynced = chartsToBeSynced.getAt(i)
+    if (choosenParams.get(chartToBeSynced)) {
+      echo "syncing service ${chartToBeSynced}"
+    } else {
+      echo "skipping service with name ${chartToBeSynced} from syncing"
+    }
+  }
+}
+
+private Map<String, Boolean> getUserInputs(List<String> charts) {
+  List<String> checkboxesForAddedCharts = []
+  for (int i = 0; i < charts.size(); i++) {
+    String addedChart = charts.get(i)
+    checkboxesForAddedCharts.add(booleanParam(defaultValue: false,
+                                              description: "Service added",
+                                              name: addedChart))
+  }
+
+  Map<String, Boolean> choosenParams = input(message: "Charts to be added",
+                                             parameters: checkboxesForAddedCharts,
+                                             submitterParameter: 'approver',
+                                             ok: "Sync services")
+
+  return choosenParams
+}
+
+private Map<String, String> getChartsFromEnv(Environment env, Cluster cluster) {
+  Map<String, String> envCharts
   DeploymentUtils deploymentUtils = new DeploymentUtils()
-  deploymentUtils.runWithK8SCliTools(firstEnv, cluster, {
+  deploymentUtils.runWithK8SCliTools(env, cluster, {
     sh "helm list --deployed | awk 'NR>1 {print \$9}' > tmpCharts"
     String charts = readFile 'tmpCharts'
-    firstEnvCharts = parseChartsIntoMap(charts)
+    envCharts = parseChartsIntoMap(charts)
   })
 
-  deploymentUtils.runWithK8SCliTools(secondEnv, cluster, {
-    sh "helm list --deployed | awk 'NR>1 {print \$9}' > tmpCharts"
-    String charts = readFile 'tmpCharts'
-    secondEnvCharts = parseChartsIntoMap(charts)
-  })
-
-  getRemovedServices(firstEnvCharts, secondEnvCharts)
-  return getModifiedServices(firstEnvCharts, secondEnvCharts)
-
+  return envCharts
 }
 
 private Map<String, String> parseChartsIntoMap(String charts) {
@@ -90,34 +112,32 @@ private Map<String, String> parseChartsIntoMap(String charts) {
   return chartsMap
 }
 
-private Map<String, String> getModifiedServices(Map<String, String> firstEnv, Map<String, String> secondEnv) {
+private List<String> getModifiedCharts(Map<String, String> firstEnv, Map<String, String> secondEnv) {
   Set<String> firstEnvCharts = firstEnv.keySet()
-  Map<String, String> modifiedCharts = new HashMap<>()
-  //todo: delete me
-  secondEnv.put("delivery-varnish", "1.2.23")
-  for (int i = 0; i < firstEnvCharts.size(); i++) {
-    String k = firstEnvCharts[i]
-    String v = firstEnv.get(k)
-    if (!secondEnv.containsKey(k)) {
-      modifiedCharts.put(k, "Service added")
-      println "${k}: service added"
-    }
+  List<String> modifiedCharts = new ArrayList<>()
 
-    if (v != secondEnv[k]) {
-      modifiedCharts.put(k, "diff between versions: ${v} -- ${secondEnv[k]}")
-      println "${k}: diff between versions: ${v} -- ${secondEnv[k]}"
+  for (int i = 0; i < firstEnvCharts.size(); i++) {
+    String chartName = firstEnvCharts[i]
+    String chartVersion = firstEnv.get(chartName)
+
+    if (secondEnv[chartName] != null && chartVersion != secondEnv[chartName]) {
+      modifiedCharts.add(chartName)
+      echo "${chartName}: updated chart: ${chartVersion} -- ${secondEnv[chartName]}"
     }
   }
 
   return modifiedCharts
 }
 
-private void getRemovedServices(Map<String, String> firstEnv, Map<String, String> secondEnv) {
+private List<String> diffBetweenMaps(Map<String, String> firstEnv, Map<String, String> secondEnv) {
   Set<String> secondEnvCharts = secondEnv.keySet()
+  List<String> removedCharts = []
   for (int i = 0; i <= secondEnvCharts.size(); i++) {
-    String k = secondEnvCharts[i]
-    if (!firstEnv.containsKey(k)) {
-      println "${k} service removed"
+    String chartName = secondEnvCharts[i]
+    if (chartName != null && !firstEnv.containsKey(chartName)) {
+      removedCharts.add(chartName)
     }
   }
+
+  return removedCharts
 }
