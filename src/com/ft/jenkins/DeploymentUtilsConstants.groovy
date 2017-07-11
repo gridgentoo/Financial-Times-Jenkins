e6c279bd112d4142c5d38a2103d0d2fd705c5ba9
Sorin Buliarca <sorin.buliarca@ft.com>
2017-07-11 15:06:01 +0300

    Fixing build after Jenkins update

--- src/com/ft/jenkins/DeploymentUtilsConstants.groovy
+++ src/com/ft/jenkins/DeploymentUtilsConstants.groovy
@@ -3,12 +3,14 @@ package com.ft.jenkins
 /**
  * There is currently no way to define these constants directly in the DeploymentUtils script, so putting them in a separate class.
  */
-final class DeploymentUtilsConstants {
-  public static String CREDENTIALS_DIR = "credentials"
-  public static String K8S_CLI_IMAGE = "coco/k8s-cli-utils:latest"
-  public static String HELM_CONFIG_FOLDER = "helm"
-  public static String HELM_CHART_LOCATION_REGEX = "${HELM_CONFIG_FOLDER}/**/Chart.yaml"
-  public static String APPS_CONFIG_FOLDER = "app-configs"
+public class DeploymentUtilsConstants {
+  public static final String CREDENTIALS_DIR = "credentials"
+  public static final String K8S_CLI_IMAGE = "coco/k8s-cli-utils:latest"
+  public static final String HELM_CONFIG_FOLDER = "helm"
+  /*  todo [sb] After a jenkins plugins update, the following line no longer works. Please try again later, as we'd like to reuse the value of a constant in other constants */
+//  public static String HELM_CHART_LOCATION_REGEX = "${HELM_CONFIG_FOLDER}/**/Chart.yaml"
+  public static final String HELM_CHART_LOCATION_REGEX =  "helm/**/Chart.yaml"
+  public static final String APPS_CONFIG_FOLDER = "app-configs"
   public static final String DEFAULT_HELM_VALUES_FILE = "values.yaml"
   public static final String OPTION_ALL = "All"
 
