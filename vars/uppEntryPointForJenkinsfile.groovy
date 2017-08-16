75d387672308635a9856cb90ed08fd6ca5497c0f
Sorin Buliarca <sorin.buliarca@ft.com>
2017-08-16 16:43:43 +0300

    Added support for multiple production envs Renamed groovy scripts

---
+++ vars/uppEntryPointForJenkinsfile.groovy
@@ -0,0 +1,16 @@
+import com.ft.jenkins.BuildConfig
+import com.ft.jenkins.Environment
+
+/**
+ * Entry point to be used in UPP repositories.
+ *
+ * It uses the generic entry point and configures it to deploy to UPP environments.
+ */
+
+def call() {
+  BuildConfig config = new BuildConfig()
+  config.setProdEnvName(Environment.PRE_PROD_NAME)
+  config.setProdEnvName(Environment.PROD_NAME)
+
+  genericEntryPointForJenkinsfile(config)
+}
