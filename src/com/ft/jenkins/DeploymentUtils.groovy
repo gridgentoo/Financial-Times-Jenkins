ad6d1010bf8e8a4c365fa2105677527330efa032
Jermila Dhas <dhas.jermila@gmail.com>
2017-08-17 09:46:45 +0100

    Pass the region as a parameter

--- src/com/ft/jenkins/DeploymentUtils.groovy
+++ src/com/ft/jenkins/DeploymentUtils.groovy
@@ -48,7 +48,7 @@ public executeAppsDeployment(Cluster targetCluster, List<String> appsToDeploy, S
 
       echo "Using app config file ${configurationFileName} to deploy with helm"
 
-      sh "helm upgrade ${app} ${chartFolderLocation} -i -f ${configurationFileName}"
+      sh "helm upgrade ${app} ${chartFolderLocation} -i -f ${configurationFileName} --set region=${region}"
     }
   })
 }
