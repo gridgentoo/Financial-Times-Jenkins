d52b3dfed891224db8e1b1f299e279ebb326b5e0
Bogdan Socaciu <bogdan.socaciu@iquestgroup.com>
2018-02-07 16:27:11 +0200

    Implemented update-cluster pipeline

---
+++ src/com/ft/jenkins/aws/ClusterManagementUtils.groovy
@@ -0,0 +1,46 @@
+package com.ft.jenkins.aws
+
+import static com.ft.jenkins.DeploymentUtilsConstants.CREDENTIALS_DIR
+
+public void updateCluster(String awsRegion, String clusterName, String clusterEnv, String envType,
+                          String platform, String vaultPass) {
+  prepareK8SCliCredentials(getFullClusterName(awsRegion, clusterName, envType, platform))
+  docker.image("k8s-provisioner:local").inside("-v \$(pwd)/credentials:/ansible/credentials " +
+          "-e 'AWS_REGION=${awsRegion}'" +
+          "-e 'CLUSTER_NAME=${clusterName}'" +
+          "-e 'CLUSTER_ENVIRONMENT=${clusterEnv}'" +
+          "-e 'ENVIRONMENT_TYPE=${envType}'" +
+          "-e 'PLATFORM=${platform}'" +
+          "-e 'VAULT_PASS=${vaultPass}'") {
+    sh "printenv"
+//      sh "update.sh"
+  }
+}
+
+public static String getFullEnvironmentType(String envType) {
+  if (envType == 'd') {
+    return "test"
+  } else if (envType == 't' || envType == 'p') {
+    return "prod"
+  }
+  return envType
+}
+
+private static String getFullClusterName(String awsRegion, String clusterName, String envType, String platform) {
+  return "${platform}.${getFullEnvironmentType(envType)}.${clusterName}.${awsRegion}"
+}
+
+private void prepareK8SCliCredentials(String fullClusterName) {
+  withCredentials([
+          [$class: 'FileBinding', credentialsId: "ft.k8s-provision.${fullClusterName}.credentials", variable: 'CREDENTIALS'],
+  ]) {
+    sh """
+      mkdir -p ${CREDENTIALS_DIR}
+      rm -f ${CREDENTIALS_DIR}/*
+      unzip ${env.CREDENTIALS} -d ${CREDENTIALS_DIR}
+    """
+  }
+}
+
+
+return this
---
+++ vars/updateCluster.groovy
@@ -0,0 +1,54 @@
+import com.ft.jenkins.aws.ClusterManagementUtils
+import com.ft.jenkins.docker.DockerUtils
+
+def call() {
+  String repoURL = 'git@github.com:Financial-Times/k8s-aws-delivery-poc.git'
+  String relativeTargetDir = 'k8s-provisioner'
+
+  String awsRegion = env."AWS region"
+  String clusterName = env."Cluster name"
+  String clusterEnvironment = env."Cluster environment"
+  String environmentType = env."Environment type"
+  String platform = env."Platform"
+  String gitBranch = env."Git branch"
+  String vaultPass = getVaultPass(ClusterManagementUtils.getFullEnvironmentType(environmentType))
+
+  ClusterManagementUtils clusterManagementUtils = new ClusterManagementUtils()
+  DockerUtils dockerUtils = new DockerUtils()
+
+  catchError {
+    node('docker') {
+      timeout(30) { //  timeout after 30 mins to not block jenkins
+        stage('checkout') {
+          checkout([$class           : 'GitSCM',
+                    branches         : [[name: gitBranch]],
+                    extensions       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: relativeTargetDir]],
+                    userRemoteConfigs: [[url: repoURL]]
+          ])
+        }
+
+        stage('build image') {
+          dockerUtils.buildImage("k8s-provisioner:${gitBranch}", relativeTargetDir)
+        }
+
+        stage('update cluster') {
+          clusterManagementUtils.updateCluster(awsRegion, clusterName, clusterEnvironment,
+                  environmentType, platform, vaultPass)
+        }
+      }
+    }
+  }
+
+  stage("cleanup") {
+    node("docker") {
+      cleanWs()
+    }
+  }
+}
+
+private String getVaultPass(String envType) {
+  withCredentials([
+          [$class: 'FileBinding', credentialsId: "ft.k8s-provision.content-${envType}.vault.pass", variable: 'VAULT_PASS']]) {
+    return env.VAULT_PASS
+  }
+}
