a33b5fbd1a5ab5842f1386c03a71579cc641c812
Sorin Buliarca <sorin.buliarca@ft.com>
2018-02-23 15:21:31 +0200

    Using the unpacked folder for the credentials.

--- src/com/ft/jenkins/aws/ClusterManagementUtils.groovy
+++ src/com/ft/jenkins/aws/ClusterManagementUtils.groovy
@@ -4,11 +4,10 @@ import static com.ft.jenkins.DeploymentUtilsConstants.CREDENTIALS_DIR
 
 public void updateCluster(String awsRegion, String clusterName, String clusterEnv, String envType,
                           String platform, String vaultPass, String gitBranch) {
-  prepareK8SCliCredentials(getFullClusterName(awsRegion, clusterEnv, envType, platform))
-  String currentDir = pwd()
+  String credentialsDir = prepareK8SCliCredentials(getFullClusterName(awsRegion, clusterEnv, envType, platform))
   String dockerRunArgs =
       "-u root " +
-      "-v ${currentDir}/${CREDENTIALS_DIR}:/ansible/credentials " +
+      "-v ${credentialsDir.trim()}:/ansible/credentials " +
       "-e 'AWS_REGION=${awsRegion}' " +
       "-e 'CLUSTER_NAME=${clusterName}' " +
       "-e 'CLUSTER_ENVIRONMENT=${clusterEnv}' " +
@@ -34,16 +33,23 @@ private static String getFullClusterName(String awsRegion, String clusterEnv, St
   return "${platform}-${getFullEnvironmentType(envType)}-${clusterEnv}-${awsRegion}"
 }
 
-private void prepareK8SCliCredentials(String fullClusterName) {
+private String prepareK8SCliCredentials(String fullClusterName) {
+  /*  unzip the TLS assets of the cluster stored as credentials in Jenkins */
   withCredentials([
       [$class: 'FileBinding', credentialsId: "ft.k8s-provision.${fullClusterName}.credentials", variable: 'CREDENTIALS'],
   ]) {
-    sh """
+    sh ("""
       mkdir -p ${CREDENTIALS_DIR}
-      rm -f ${CREDENTIALS_DIR}/*
+      rm -rf ${CREDENTIALS_DIR}/*
       unzip ${env.CREDENTIALS} -d ${CREDENTIALS_DIR}
-    """
+    """)
   }
+
+  //  get the absolute path of the parent dir of the ca.pem file
+  return sh (returnStdout: true, script: """
+      cafile=\$(find ${CREDENTIALS_DIR} -name ca.pem)
+      echo "\$(cd \$(dirname \${cafile}); pwd -P)" 
+    """)
 }
 
 return this
