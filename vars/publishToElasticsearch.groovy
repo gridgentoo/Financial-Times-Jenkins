3d68153574a7718554800115ab8c7dcb571b608f
Bogdan Socaciu <bogdan.socaciu@ft.com>
2018-03-29 17:43:37 +0300

    Minor fixes

--- vars/publishToElasticsearch.groovy
+++ vars/publishToElasticsearch.groovy
@@ -3,6 +3,7 @@ import com.ft.jenkins.*
 def call() {
   ParamUtils paramUtils = new ParamUtils()
   String environmentInput = paramUtils.getRequiredParameterValue("Environment")
+  String regionInput = paramUtils.getRequiredParameterValue("Region")
   String cmsInput = paramUtils.getRequiredParameterValue("CMS")
 
   Environment targetEnv = computeTargetEnvironment(environmentInput)
@@ -10,49 +11,59 @@ def call() {
 
   String GET_MONGO_CONTAINER_CMD = "kubectl get pods | grep mongodb | awk '{print \$1}' | head -1"
 
-  String GET_METHODE_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"mediaType\":null,\"identifiers.authority\": {$regex: /FTCOM-METHODE/ },\"type\": {$nin: [\"ContentPackage\",\"Content\",\"ImageSet\"]}}, {_id: false, uuid: 1}).forEach(function(o) { print(o.uuid)})' --quiet > uuids.txt "
-  String GET_WORDPRESS_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"mediaType\":null,\"identifiers.authority\": {$regex: /FT-LABS/ }}, {_id: false, uuid: 1}).forEach(function(o) { printjson(o)})' --quiet > uuids.txt"
-  String GET_VIDEO_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"identifiers.authority\": {$regex: /NEXT-VIDEO-EDITOR/ }}, {_id: false, uuid: 1}).forEach(function(o) { printjson(o)})' --quiet > uuids.txt"
+  String GET_METHODE_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"mediaType\":null,\"identifiers.authority\": {\$regex: /FTCOM-METHODE/ },\"type\": {\$nin: [\"ContentPackage\",\"Content\",\"ImageSet\"]}}, {_id: false, uuid: 1}).forEach(function(o) { print(o.uuid)})' --quiet > uuids.txt "
+  String GET_WORDPRESS_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"mediaType\":null,\"identifiers.authority\": {\$regex: /FT-LABS/ }}, {_id: false, uuid: 1}).forEach(function(o) { printjson(o)})' --quiet | jq -r '.uuid' > uuids.txt"
+  String GET_VIDEO_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"identifiers.authority\": {\$regex: /NEXT-VIDEO-EDITOR/ }}, {_id: false, uuid: 1}).forEach(function(o) { printjson(o)})' --quiet | jq -r '.uuid' >> uuids.txt"
 
-  Closure SERVICE_SHUTDOWN = { sh "kubectl --scale=0 content-rw-elasticsearch" }
-  Closure SERVICE_STARTUP = { sh "kubectl --scale=2 content-rw-elasticsearch" }
+  Closure SERVICE_SHUTDOWN = { sh "kubectl scale --replicas=0 deployments/content-rw-elasticsearch" }
+  Closure SERVICE_STARTUP = { sh "kubectl scale --replicas=2 deployments/content-rw-elasticsearch" }
 
   Closure SETUP_ENDPOINT_HITTER = {
     sh "apk add go"
     sh "go get -u github.com/Financial-Times/endpoint-hitter"
   }
-  Closure CALL_ENDPOINT_HITTER = { sh "endpoint-hitter --target-url=${targetEnv.name}" }
+  Closure CALL_ENDPOINT_HITTER = { sh "endpoint-hitter --target-url=${targetEnv.getFullClusterName(Cluster.DELIVERY, regionInput)}" }
   Closure GET_METHODE_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_METHODE_UUIDS_CMD}" }
   Closure GET_WORDPRESS_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_WORDPRESS_UUIDS_CMD}" }
   Closure GET_VIDEO_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_VIDEO_UUIDS_CMD}" }
 
   node('docker') {
-    stage('Disable content-rw-elasticsearch') {
-      deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, null, SERVICE_SHUTDOWN)
-      sleep(3000) // Wait for content-rw-elasticsearch to be disabled
-    }
-    stage('Delete ES index') {
-//      sh "curl -XDELETE 'localhost:9200/twitter?pretty'"
-    }
-    stage('Enable content-rw-elasticsearch') {
-      deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, null, SERVICE_STARTUP)
-    }
-    stage('Get UUIDs and publish content') {
-      switch (cmsInput) {
-        case "Methode":
-          def methodeCall = SETUP_ENDPOINT_HITTER >> GET_METHODE_UUIDS >> CALL_ENDPOINT_HITTER
-          deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, null, methodeCall)
-          break
-        case "Wordpress":
-          def wordpressCall = SETUP_ENDPOINT_HITTER >> GET_WORDPRESS_UUIDS >> CALL_ENDPOINT_HITTER
-          deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, null, wordpressCall)
-          break
-        case "Video":
-          def videoCall = SETUP_ENDPOINT_HITTER >> GET_VIDEO_UUIDS >> CALL_ENDPOINT_HITTER
-          deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, null, videoCall)
-          break
-        default: error("CMS parameter value is unknown!")
+    catchError {
+      stage('Disable content-rw-elasticsearch') {
+        deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, SERVICE_SHUTDOWN)
+        sleep(5) // Wait 5s for content-rw-elasticsearch to be disabled
       }
+
+      stage('Delete ES index') {
+        echo "Deleting ES index"
+//      sh "curl -XDELETE 'localhost:9200/twitter?pretty'"
+      }
+
+      stage('Enable content-rw-elasticsearch') {
+        deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, SERVICE_STARTUP)
+      }
+
+      stage('Get UUIDs and publish content') {
+        switch (cmsInput) {
+          case "Methode":
+            def methodeCall = SETUP_ENDPOINT_HITTER >> GET_METHODE_UUIDS >> CALL_ENDPOINT_HITTER
+            deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, methodeCall)
+            break
+          case "Wordpress":
+            def wordpressCall = SETUP_ENDPOINT_HITTER >> GET_WORDPRESS_UUIDS >> CALL_ENDPOINT_HITTER
+            deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, wordpressCall)
+            break
+          case "Video":
+            def videoCall = SETUP_ENDPOINT_HITTER >> GET_VIDEO_UUIDS >> CALL_ENDPOINT_HITTER
+            deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, videoCall)
+            break
+          default: error("CMS parameter value is unknown!")
+        }
+      }
+    }
+
+    stage('cleanup') {
+      cleanWs()
     }
   }
 }
