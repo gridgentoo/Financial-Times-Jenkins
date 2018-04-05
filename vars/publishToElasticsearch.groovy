97ff4ca434f86f5c9c4e0deb30ef0662c4642760
Bogdan Socaciu <bogdan.socaciu@ft.com>
2018-04-05 11:56:10 +0300

    Added cluster deletion

--- vars/publishToElasticsearch.groovy
+++ vars/publishToElasticsearch.groovy
@@ -5,6 +5,7 @@ def call() {
   String environmentInput = paramUtils.getRequiredParameterValue("Environment")
   String regionInput = paramUtils.getRequiredParameterValue("Region")
   String cmsInput = paramUtils.getRequiredParameterValue("CMS")
+  String indexInput = paramUtils.getRequiredParameterValue("Index")
 
   Environment targetEnv = computeTargetEnvironment(environmentInput)
   DeploymentUtils deployUtil = new DeploymentUtils()
@@ -16,13 +17,23 @@ def call() {
   String GET_VIDEO_UUIDS_CMD = "mongo upp-store -eval 'rs.slaveOk(); db.content.find({\"identifiers.authority\": {\$regex: /NEXT-VIDEO-EDITOR/ }}, {_id: false, uuid: 1}).forEach(function(o) { printjson(o)})' --quiet | jq -r '.uuid' >> uuids.txt"
 
   Closure SERVICE_SHUTDOWN = { sh "kubectl scale --replicas=0 deployments/content-rw-elasticsearch" }
+  Closure INDEX_DELETION = {
+    sh "apk add go"
+    sh "go get -u github.com/Financial-Times/elasticsearch-index-zapper"
+    sh "elasticsearch-index-zapper --aws-access-key=`kubectl get secret global-secrets -o yaml | grep aws.access_key_id | head -1 | awk '{print \$2}' | base64 --decode` " +
+            "--aws-secret-access-key=`kubectl get secret global-secrets -o yaml | grep aws.secret_access_key | head -1 | awk '{print \$2}' | base64 --decode` " +
+            "--elasticsearch-endpoint=`kubectl get configmap global-config -o yaml | grep aws.content.elasticsearch.endpoint | awk '{print \$2}'`" +
+            "--elasticsearch-index='${indexInput}'"
+  }
   Closure SERVICE_STARTUP = { sh "kubectl scale --replicas=2 deployments/content-rw-elasticsearch" }
 
   Closure SETUP_ENDPOINT_HITTER = {
     sh "apk add go"
     sh "go get -u github.com/Financial-Times/endpoint-hitter"
   }
-  Closure CALL_ENDPOINT_HITTER = { sh "endpoint-hitter --target-url=${targetEnv.getFullClusterName(Cluster.DELIVERY, regionInput)}" }
+  Closure CALL_ENDPOINT_HITTER = {
+    sh "endpoint-hitter --target-url=${targetEnv.getFullClusterName(Cluster.DELIVERY, regionInput)}"
+  }
   Closure GET_METHODE_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_METHODE_UUIDS_CMD}" }
   Closure GET_WORDPRESS_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_WORDPRESS_UUIDS_CMD}" }
   Closure GET_VIDEO_UUIDS = { sh "kubectl exec -it `${GET_MONGO_CONTAINER_CMD}` -- ${GET_VIDEO_UUIDS_CMD}" }
@@ -35,8 +46,7 @@ def call() {
       }
 
       stage('Delete ES index') {
-        echo "Deleting ES index"
-//      sh "curl -XDELETE 'localhost:9200/twitter?pretty'"
+        deployUtil.runWithK8SCliTools(targetEnv, Cluster.DELIVERY, regionInput, INDEX_DELETION)
       }
 
       stage('Enable content-rw-elasticsearch') {
@@ -68,7 +78,7 @@ def call() {
   }
 }
 
-private Environment computeTargetEnvironment(String environmentInput) {
+private static Environment computeTargetEnvironment(String environmentInput) {
   Environment targetEnv = EnvsRegistry.getEnvironment(environmentInput)
   if (targetEnv == null) {
     throw new IllegalArgumentException("Unknown environment ${environmentInput}. The environment is required.")
