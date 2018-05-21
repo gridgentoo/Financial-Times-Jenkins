134215f7197df56bb05f9b11fed25bd29d77b4af
Tamas Molnar <tamas.molnar@ft.com>
2018-05-21 17:01:45 +0300

    started to add CR support for update dex configs

--- vars/updateDexConfigs.groovy
+++ vars/updateDexConfigs.groovy
@@ -2,6 +2,11 @@ import com.ft.jenkins.Cluster
 import com.ft.jenkins.DeploymentUtils
 import com.ft.jenkins.Environment
 import com.ft.jenkins.EnvsRegistry
+import com.ft.jenkins.changerequests.ChangeRequestCloseData
+import com.ft.jenkins.changerequests.ChangeRequestEnvironment
+import com.ft.jenkins.changerequests.ChangeRequestOpenData
+import com.ft.jenkins.changerequests.ChangeRequestsUtils
+import com.ft.jenkins.git.GithubReleaseInfo
 import com.ft.jenkins.provision.ClusterUpdateInfo
 import com.ft.jenkins.provision.ProvisionerUtil
 
@@ -25,13 +30,16 @@ private void updateDexConfig() {
     List<String> clusters = new ArrayList<>()
     clusters.addAll((HashSet<String>) configMap.keySet())
     List<String> selectedClusters = new ArrayList<>()
+    String approver = ""
 
     stage('Select clusters to update') {
-        selectedClusters = getSelectedUserInputs(clusters,
+        (selectedClusters, approver) = getSelectedUserInputs(clusters,
                 "Clusters to deploy dex-config to",
                 "Update dex-config")
         echo "The following charts were selected for adding: ${selectedClusters}"
     }
+
+    String crId = openCr(approver, app, environment)
 
     selectedClusters.each { String clusterName ->
         Map<String, String> secrets = configMap."$clusterName"
@@ -68,7 +76,7 @@ private void updateDexConfig() {
             appVersion = scmVars.GIT_COMMIT.take(7)
 
             String valuesFile = "values.yaml"
-            writeFile([file: valuesFile, text: buildHelmValues2(secrets, clusterName)])
+            writeFile([file: valuesFile, text: buildHelmValues(secrets, clusterName)])
 
             String helmDryRunOutput = "output.txt"
             deploymentUtils.runWithK8SCliTools(targetEnv, targetCluster, targetRegion, {
@@ -96,12 +104,50 @@ private void updateDexConfig() {
     }
 }
 
+private String openCr(String approver, String chartName, Environment environment) {
+    try {
+        ChangeRequestOpenData data = new ChangeRequestOpenData()
+        data.ownerEmail = "${approver}@ft.com"
+        data.summary = "Deploying chart ${chartName}@master with apps ${computeSimpleTextForAppsToDeploy(appsPerCluster)} in ${environment.name}"
+        data.description = releaseInfo.title
+        data.details = releaseInfo.title
+        data.environment = environment.name == Environment.PROD_NAME ? ChangeRequestEnvironment.Production :
+                ChangeRequestEnvironment.Test
+        data.notifyChannel = environment.slackChannel
+        data.notify = true
+
+        ChangeRequestsUtils crUtils = new ChangeRequestsUtils()
+        return crUtils.open(data)
+    }
+    catch (e) { //  do not fail if the CR interaction fail
+        echo "Error while opening CR for release ${releaseInfo.getTagName()}: ${e.message} "
+    }
+}
+
+private void closeCr(String crId, Environment environment) {
+    if (crId == null) {
+        return
+    }
+
+    try {
+        ChangeRequestCloseData data = new ChangeRequestCloseData()
+        data.notifyChannel = environment.slackChannel
+        data.id = crId
+
+        ChangeRequestsUtils crUtils = new ChangeRequestsUtils()
+        crUtils.close(data)
+    }
+    catch (e) { //  do not fail if the CR interaction fail
+        echo "Error while closing CR ${crId}: ${e.message} "
+    }
+}
+
 private Map<String, Map<String, String>> readConfig() {
     def configMap = readJSON text: env."Dex config"
     (Map<String, Map<String, String>>) configMap
 }
 
-private List<String> getSelectedUserInputs(List<String> clusters, String inputMessage,
+private def getSelectedUserInputs(List<String> clusters, String inputMessage,
                                            String okButton) {
     List checkboxes = []
     clusters.sort()
@@ -120,9 +166,9 @@ private List<String> getSelectedUserInputs(List<String> clusters, String inputMe
             parameters: checkboxes,
             submitterParameter: APPROVER_INPUT,
             ok: okButton) as Map<String, Object>
-    extractApprover(rawUserInputs)
 
-    return getSelectedValues(rawUserInputs)
+
+    return getSelectedValues(rawUserInputs), extractApprover(rawUserInputs)
 }
 
 private List<String> getSelectedValues(Map<String, Object> userInputs) {
@@ -177,7 +223,7 @@ private Object checkoutDexConfig(String app) {
     ])
 }
 
-private String buildHelmValues2(Map<String, String> secrets, String clusterName) {
+private String buildHelmValues(Map<String, String> secrets, String clusterName) {
     String helmValues = "github:\n  client:\n    id: " + '"' + secrets."github.client.id" + '"' +
             "\n    secret: " + '"' + secrets."github.client.secret" + '"' + "\nkubectl:\n  login:\n    secret: " +
             '"' + secrets."kubectl.login.secret" + '"' + "\ncluster:\n  name: " + '"' + clusterName + '"' +
