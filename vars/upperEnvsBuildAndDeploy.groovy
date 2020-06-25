2775035f136d8183e2e5340c89a5b4d726b1eb9b
Tsvetan Dimitrov <tsvetan.dimitrov@ft.com>
2020-06-25 17:16:41 +0300

    UPPSF-1260 Fixed triggering auto deploy on dev only when a cluster supports dev env

--- vars/upperEnvsBuildAndDeploy.groovy
+++ vars/upperEnvsBuildAndDeploy.groovy
@@ -64,11 +64,13 @@ void autoDeployToEnvironment(String targetEnvName, String chartName,
                              int daysForTheDeployment) {
   Set<ClusterType> availableClusterTypes = appsPerCluster.keySet().findAll { it != ClusterType.UNKNOWN }
   List<Environment> availableEnvironments = availableClusterTypes.collect { EnvsRegistry.getEnvironment(it, targetEnvName) }
-  // get first non null environment
-  Environment environment = availableEnvironments.find { it }
-  stage("autodeploy to ${environment.name}") {
-    timeout(time: daysForTheDeployment, unit: 'DAYS') {
-      deployAppsToEnvironmentRegions(environment.regions, chartName, version, environment)
+  if (availableEnvironments) {
+    // get first non null environment
+    Environment environment = availableEnvironments.find { it }
+    stage("autodeploy to ${environment.name}") {
+      timeout(time: daysForTheDeployment, unit: 'DAYS') {
+        deployAppsToEnvironmentRegions(environment.regions, chartName, version, environment)
+      }
     }
   }
 }
